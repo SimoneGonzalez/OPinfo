d6c8ed80b3c3c2dc4c1edbf3a8331b25d10addcb
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2015-07-03 14:43:33 -0500

    stuff

--- src/js/assets/auth/auth.config.js
+++ src/js/assets/auth/auth.config.js
@@ -2,6 +2,9 @@ angular
   .module('opinfo')
   .config(function($routeProvider, $locationProvider) {
     $routeProvider
+      .when('/', {
+        templateUrl: 'index.html'
+      })
       .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginCtrl',
